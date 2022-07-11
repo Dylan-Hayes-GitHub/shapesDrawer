243ec11480947f743e05772724313573d613ff12
Dylan Hayes <dylanmhayes12@gmail.com>
2022-07-11 20:14:01 +0100

    Updated Logic

--- src/shape/shape.service.ts
+++ src/shape/shape.service.ts
@@ -48,14 +48,7 @@ export class ShapeService {
                           for (let innerCol = col; innerCol <= innerColControl; innerCol++) {
                               //the reason to check innerrow and innerCol in comparison to their controls is to prevent an edge case happening when the matrix is on its last row/col
                               //check and it finds a marked slot this is fine so we prevent the matrix from finding a new position by checking if we are at the end of the search
-                              if(height === 1 && width === 1 && !this.shapesArray[row][col]){
-
-                                shapes.endingX = shapes.startingX + 1;
-                                shapes.endingY = shapes.startingY + 1;
-                                break innerLoop;
-                              }
-                              else 
-                              if ((this.shapesArray[innerRow][innerCol]) ) {
+                              if ((this.shapesArray[innerRow][innerCol] && innerCol != innerColControl && innerRow != innerRowControl ) ) {
                                   result = false;
                                   //we reset canItFit to 0, 0, 0, 0 we dont want to remember old points that the matrix had found and prevent breaking of validation errors
                                   shapes.startingX = 0;
@@ -64,7 +57,7 @@ export class ShapeService {
                                   shapes.endingY = 0;
                                   break innerLoop;
                               }
-                              else if (!this.shapesArray[innerRow][innerCol]) {
+                              else{
                                   shapes.endingX = innerCol;
                                   shapes.endingY = innerRow;
                               }
